9a989422797e07240a6302f89b777d84b53dc5a1
shin <xchen_2020@hotmail.com>
2022-05-13 15:32:57 +0200

    save clip_test

--- clip_test.py
+++ clip_test.py
@@ -8,7 +8,8 @@ import argparse
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--file_path',type = str,\
-        default='/home/xinyi/Language_Reasoning_Test/input/test.txt', help = "path of caption input")
+        default='/home/xinyi/Language_Reasoning_Test/datasets/dataset_final/active_passive_captions_gruen_strict.json', \
+        help = "path of caption input")
 
     args = parser.parse_args()
 
@@ -17,41 +18,31 @@ if __name__ == "__main__":
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print(f"device is {device}")
     clip_model, clip_preprocess = clip.load('ViT-B/32', device)
-    ranks = []
-    scores = []
+    # ranks = []
+    # scores = []
+    clip_results = []
+
     for item in items:
         image_id = item['image_id']
         captions = item['caption_group'][0]
         img_path = get_img_path(image_id)
         image = show_image(img_path)
 
-        rank, clip_score = rank_captions(image, [captions['True1'], item['True2'], \
+        rank, clip_score = rank_captions(image, [captions['True1'],captions['True2'], \
             captions['False1'], captions['False2']], clip_model, clip_preprocess, device)
-        ranks.append(rank)
-        scores.append(scores)
-    
-    active_acc = 0
-    rank_count = {}
-    for rank in ranks:
-        active_acc += get_accuracy(rank)
-        str_rank = [str(r) for r in rank]
-        rank_key = ''.join(str_rank)
-        if rank_key in rank_count:
-            rank_count[rank_key] += 1
+        if "predicate" in captions:
+            clip_results.append({"image_id": image_id, "rank": rank, \
+                "predicate": captions["predicate"]})
         else:
-            rank_count[rank_key] = 1
-
-    print(f"Accuracy is {active_acc / len(ranks)}")
-    print(sorted(rank_count.items(), key= lambda x:x[1], reverse=True))
-
-    rank_c = {}
-    for r, c in rank_count.items():
-        rank_c[r] = c
+            clip_results.append({"image_id": image_id, "rank": rank})
     
     print(args.file_path)
-    dir = "/home/xinyi/Language_Reasoning_Test/output"
+    dir = "/home/xinyi/Language_Reasoning_Test/output/clip_results"
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+        
     out_file = os.path.join(dir, \
-        args.file_path.split('/')[-1][:-5] + "_ranks"+".json")
-    write_file(out_file, rank_c)
+        args.file_path.split('/')[-1][:-5] + "_clip_results"+".json")
+    write_file(out_file, clip_results)
     print("finish\n")
     # write_file("active_scores.txt", scores)
